ab4083328b3a82d33af5363ed543fcfbc3568c52
Noah Milberger <n.milberger@gmail.com>
2022-02-28 18:23:41 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,49 @@
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
+  type Query {
+    totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
+  }
+`;
+
+var _id = 0;
+var photos = [];
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
+    },
+  },
+  Photo: {
+    url: (parent) => `https://yoursite.com/img/${parent.id}.jpg`,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`🚀 Server ready at ${url}`));
